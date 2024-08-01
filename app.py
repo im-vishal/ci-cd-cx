c010e90e8ab16e732f559927f42c4b1a49338eae
im-vishal <iamvirtualvishal@gmail.com>
2024-08-01 23:08:51 +0530

    unit-testing

--- app.py
+++ app.py
@@ -6,5 +6,12 @@ def sub(a, b):
     return a-b
 
 
-result1 = add(5, 4)
-result2 = sub(5, 1)
+def mul(a, b):
+    return a*b
+
+
+if __name__ == "__main__":
+    result1 = add(4, 5)
+    result2 = sub(4, 1)
+
+    print(result1, result2)
