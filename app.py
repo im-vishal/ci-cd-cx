1678b0cf44383f6e31c63ded8cc147c9e6b3fd99
im-vishal <iamvirtualvishal@gmail.com>
2024-08-01 21:23:10 +0530

    fixed linting issues

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-def add(a,b):
-    return a+b
+def add(a, b):
+    return a + b
 
+def sub(a, b):
+    return a-b
 
+result1 = add(5, 4)
+result2 = sub(5, 1)
 
-
-
-def sub(a, b):return a-b
-
-result1 = add(5,4)
-result2 = sub(5,1)
